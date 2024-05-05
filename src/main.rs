31e9a028a6010a206367ba214f13afeb0fa34727
john <thatbotjohn@gmail.com>
2024-05-05 15:05:15 +0100

    Writer impl for safe VGA buffer writing
    Includes print! & println! macros,
    allowing for nicer panics and easier writing to the display:)

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,20 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    print!("{}", info);
     loop {}
 }
 
-static MSG: &[u8] = b"Hey There Dad!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    const VGA_BUFFER: *mut u8 = 0xB8000 as *mut u8;
-    
-    for (i, &byte) in MSG.iter().enumerate() {
-        unsafe {
-            *VGA_BUFFER.offset(i as isize * 2) = byte;
-            *VGA_BUFFER.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-    
+    println!("Hello World! {}", "1.234");
+    panic!("AAAHHHHHHH the world is on fire!!!!");
     loop {}
 }
