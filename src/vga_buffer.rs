6628fb08348cd8708080385386ad758261aee203
john <thatbotjohn@gmail.com>
2024-05-10 15:19:36 +0100

    Add testing framework!

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -141,3 +141,25 @@ pub fn _print(args: fmt::Arguments) {
     use core::fmt::Write;
     WRITER.lock().write_fmt(args).unwrap();
 }
+
+#[test_case]
+fn test_println_simple() {
+    println!("test_println_simple output");
+}
+
+#[test_case]
+fn test_println_many() {
+    for _ in 0..200 {
+        println!("test_println_many output");
+    }
+}
+
+#[test_case]
+fn test_println_output() {
+    let s = "This is a test string that's less than one line long!";
+    println!("{}", s);
+    for (i, c) in s.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+        assert_eq!(char::from(screen_char.character_code), c);
+    }
+}
--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,6 +1,5 @@
 #![no_std]
 #![no_main]
-
 #![feature(custom_test_frameworks)]
 #![test_runner(stegosauros::test_runner)]
 #![reexport_test_harness_main = "test_main"]
@@ -10,23 +9,17 @@ use stegosauros::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World! {}", "1.234");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("\nError: {}\n", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     stegosauros::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output\n");
 }
